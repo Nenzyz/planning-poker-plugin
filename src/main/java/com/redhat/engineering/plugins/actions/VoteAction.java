d55207cd5a5242cb1d0a29ec80b97a069701eca3
Yan Valuyskiy <yan.valuyskiy@gmail.com>
2025-11-11 00:27:02 +0200

    feat: add endSession and applyEstimate actions to VoteAction

--- src/main/java/com/redhat/engineering/plugins/actions/VoteAction.java
+++ src/main/java/com/redhat/engineering/plugins/actions/VoteAction.java
@@ -1,5 +1,8 @@
 package com.redhat.engineering.plugins.actions;
 
+import com.atlassian.jira.security.request.RequestMethod;
+import com.atlassian.jira.security.request.SupportedMethods;
+
 import com.atlassian.jira.avatar.Avatar;
 import com.atlassian.jira.avatar.AvatarService;
 import com.atlassian.jira.plugin.userformat.UserFormats;
@@ -15,12 +18,24 @@ import com.redhat.engineering.plugins.services.ConfigService;
 import com.redhat.engineering.plugins.services.SessionService;
 import com.redhat.engineering.plugins.services.VoteService;
 
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.ModifiedValue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder;
+import com.atlassian.jira.component.ComponentAccessor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author vdedik@redhat.com
  */
+@SupportedMethods({RequestMethod.GET, RequestMethod.POST})
 public class VoteAction extends AbstractAction {
 
     private final JiraAuthenticationContext authContext;
@@ -36,6 +51,8 @@ public class VoteAction extends AbstractAction {
     private String voteVal;
     private String voteComment;
     private List<String> allowedVotes;
+    private String action; // "vote", "endSession", "applyEstimate"
+    private String finalValue;
 
     public VoteAction(JiraAuthenticationContext authContext, SessionService sessionService,
                       VoteService voteService, UserFormats userFormats, AvatarService avatarService,
@@ -71,6 +88,22 @@ public class VoteAction extends AbstractAction {
 
     public void setVoteComment(String voteComment) {
         this.voteComment = voteComment;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getFinalValue() {
+        return finalValue;
+    }
+
+    public void setFinalValue(String finalValue) {
+        this.finalValue = finalValue;
     }
 
     public List<String> getAllowedVotes() {
@@ -111,6 +144,16 @@ public class VoteAction extends AbstractAction {
 
     @Override
     public String doExecute() throws Exception {
+        if ("endSession".equals(action)) {
+            return doEndSession();
+        } else if ("applyEstimate".equals(action)) {
+            return doApplyEstimate();
+        } else {
+            return doVote();
+        }
+    }
+
+    private String doVote() throws Exception {
         if (!permissionManager.hasPermission(Permissions.EDIT_ISSUE, getSessionObject().getIssue(), getCurrentUser())) {
             addErrorMessage("You don't have permission to vote.");
             return ERROR;
@@ -124,6 +167,51 @@ public class VoteAction extends AbstractAction {
         voteService.save(vote);
 
         this.addMessage("Your vote has been successfully saved.");
+        return SUCCESS;
+    }
+
+    private String doEndSession() throws Exception {
+        Session session = getSessionObject();
+        if (!session.getAuthor().equals(getCurrentUser())) {
+            addErrorMessage("Only the session creator can end the session.");
+            return ERROR;
+        }
+
+        // Set end date to NOW to close session immediately
+        session.setEnd(new Date());
+        sessionService.update(session);
+
+        addMessage("Session ended successfully.");
+        return SUCCESS;
+    }
+
+    private String doApplyEstimate() throws Exception {
+        Session session = getSessionObject();
+        if (!session.getAuthor().equals(getCurrentUser())) {
+            addErrorMessage("Only the session creator can apply estimates.");
+            return ERROR;
+        }
+
+        if (System.currentTimeMillis() < session.getEnd().getTime()) {
+            addErrorMessage("Cannot apply estimate until session is ended.");
+            return ERROR;
+        }
+
+        // Update customfield_10205
+        Issue issue = session.getIssue();
+        CustomField estimateField = ComponentAccessor.getCustomFieldManager()
+            .getCustomFieldObject("customfield_10205");
+
+        if (estimateField != null) {
+            MutableIssue mutableIssue = ComponentAccessor.getIssueManager()
+                .getIssueObject(issue.getId());
+            Double value = Double.parseDouble(getFinalValue());
+            estimateField.updateValue(null, mutableIssue,
+                new ModifiedValue(mutableIssue.getCustomFieldValue(estimateField), value),
+                new DefaultIssueChangeHolder());
+        }
+
+        addMessage("Estimate " + getFinalValue() + " applied to issue.");
         return SUCCESS;
     }
 
@@ -190,5 +278,50 @@ public class VoteAction extends AbstractAction {
 
     private ApplicationUser getCurrentUser() {
         return authContext.getUser();
+    }
+
+    public Map<String, Object> getSessionStats() {
+        Session session = getSessionObject();
+        List<Vote> votes = voteService.getVotesBySession(session);
+
+        List<Double> numericVotes = new ArrayList<>();
+        for (Vote vote : votes) {
+            String val = vote.getValue();
+            if (val != null && val.matches("\\d+(\\.\\d+)?")) {
+                numericVotes.add(Double.parseDouble(val));
+            }
+        }
+
+        if (numericVotes.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        double sum = 0;
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+
+        for (Double v : numericVotes) {
+            sum += v;
+            if (v < min) min = v;
+            if (v > max) max = v;
+        }
+
+        Map<String, Object> stats = new HashMap<>();
+        stats.put("min", min);
+        stats.put("max", max);
+        stats.put("average", sum / numericVotes.size());
+        stats.put("count", numericVotes.size());
+
+        return stats;
+    }
+
+    public boolean isCreator() {
+        Session session = getSessionObject();
+        return session != null && session.getAuthor().equals(getCurrentUser());
+    }
+
+    public boolean isSessionEnded() {
+        Session session = getSessionObject();
+        return session != null && System.currentTimeMillis() >= session.getEnd().getTime();
     }
 }
